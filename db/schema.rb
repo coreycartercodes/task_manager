df3bc90461e949886dfb4d7b0503e4dacaf76354
Corey Carter <crc2231@gmail.com>
2020-08-07 03:09:37 -0600

    Final Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_08_07_084304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+  end
 
 end
